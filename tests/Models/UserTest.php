dd6a1651ace896d96d23c65f5eedcbbf93f858e3
gaellerem <146330927+gaellerem@users.noreply.github.com>
2025-06-25 16:31:54 +0200

    unit tests for remember me token

--- tests/Models/UserTest.php
+++ tests/Models/UserTest.php
@@ -43,4 +43,57 @@ class UserTest extends TestCase {
 
         $this->assertEquals($expectedUser, $result);
     }
+    public function testStoreRememberTokenUpdatesUser()
+    {
+        $stmt = $this->createMock(PDOStatement::class);
+        $stmt->expects($this->exactly(2))
+            ->method('bindParam');
+
+        $stmt->expects($this->once())
+            ->method('execute');
+
+        $pdo = $this->createMock(PDO::class);
+        $pdo->expects($this->once())
+            ->method('prepare')
+            ->with("UPDATE users SET remember_token = :token WHERE id = :id")
+            ->willReturn($stmt);
+
+        User::setDB($pdo);
+
+        User::storeRememberToken(1, 'random_token');
+    }
+
+    public function testGetByRememberTokenReturnsUser()
+    {
+        $expectedUser = [
+            'id' => 1,
+            'username' => 'testuser',
+            'email' => 'test@example.com',
+            'remember_token' => 'valid_token'
+        ];
+
+        $stmt = $this->createMock(PDOStatement::class);
+        $stmt->expects($this->once())
+            ->method('bindParam')
+            ->with(':token', 'valid_token');
+
+        $stmt->expects($this->once())
+            ->method('execute');
+
+        $stmt->expects($this->once())
+            ->method('fetch')
+            ->with(PDO::FETCH_ASSOC)
+            ->willReturn($expectedUser);
+
+        $pdo = $this->createMock(PDO::class);
+        $pdo->expects($this->once())
+            ->method('prepare')
+            ->willReturn($stmt);
+
+        User::setDB($pdo);
+
+        $result = User::getByRememberToken('valid_token');
+
+        $this->assertEquals($expectedUser, $result);
+    }
 }
